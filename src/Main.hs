36eab1e1bebca086c8d92782f0e35dce29898309
Ryan Trinkle <ryan.trinkle@gmail.com>
2017-01-27 15:08:00 -0500

    Use fromString to make the example work with all versions of reflex-dom

--- src/Main.hs
+++ src/Main.hs
@@ -3,28 +3,31 @@
 module Main where
 
 import Control.Monad
+import Control.Monad.IO.Class
 import Data.String.Class
-import Data.Text (Text)
 import Reflex
 import Reflex.Dom
+import System.Exit
 
 main :: IO ()
 main =
   mainWidget $ do
-    clicked <- button "Go"
+    clicked <- delay 0.1 =<< getPostBuild
+    done <- delay 10 clicked
+    performEvent_ $ liftIO exitSuccess <$ done
     void . widgetHold blank $ ffor clicked $ \_ -> do
       let colNames = ["a","b","c","d"]
       elClass "table" "table" $ do
         el "thead" . el "tr" $ forM_ colNames $ \n ->
             el "th" (text n)
         el "tbody" . forM_ table $ \Entry{..} -> do
-          let cells = [a, b, toText $ show c, toText $ show d]
+          let cells = fromString <$> [a, b, show c, show d]
           el "tr" . forM_ cells $ \cell -> do
                 el "td" (text cell)
 
 data Entry = Entry
-  { a :: Text
-  , b :: Text
+  { a :: String
+  , b :: String
   , c :: Double
   , d :: Int
   }
